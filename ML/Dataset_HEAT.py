68e7dd7919522cf9f9fb7a2028765b7625b40736
CEbuddies <68404468+CEbuddies@users.noreply.github.com>
2021-03-06 09:47:49 +0100

    Create Dataset_HEAT.py
    Added a Dataloader

---
+++ ML/Dataset_HEAT.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  2 15:40:34 2021
+
+@author: User
+"""
+
+import torch 
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import pickle
+
+class HEAT_Data(Dataset):
+    
+    def __init__(self):
+        
+        with open('smaples_tars.trn','rb') as pl:
+            xy = pickle.load(pl)
+        self.x = xy['samples']
+        # alle x mal 1000 nehmen 
+        self.y = xy['targets']
+        self.nums = len(self.x)
+        
+    def __getitem__(self,index):
+        # string create 
+        return self.x[index], self.y[index]
+        
+    def __len__(self):
+        
+        return self.nums
+    
+# use this in the pytorch lightning module
+dataset = HEAT_Data()
+dataloader = DataLoader(dataset=dataset,
+                        batch_size=16,shuffle=True,num_workers=2)
+        
