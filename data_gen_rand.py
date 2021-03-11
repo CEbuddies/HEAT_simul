e06a3ae05b2156f619f22f725791950b277ba3c6
CEbuddies <68404468+CEbuddies@users.noreply.github.com>
2021-03-11 11:55:32 +0100

    Create data_gen_rand.py
    Random BCs at sides

---
+++ data_gen_rand.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 11:49:30 2021
+
+@author: nwilhelm
+"""
+
+from SimulSetup import Simulation
+import pickle
+import numpy as np
+
+# Script for the data generation 
+
+def data_gen():
+    
+    el_side = 40
+    
+    locs = ['left','right','top','bottom']
+    bc_left = np.random.rand(el_side,1)
+    bc_right = np.random.rand(el_side,1)
+    
+    bc_top = np.random.rand(el_side,1)
+    bc_bottom = np.random.rand(el_side,1)
+    
+    
+    bc_vals = [bc_left,bc_right,bc_top,bc_bottom]
+    bc_dict = {'locations':locs,'values':bc_vals}
+    
+    sim = Simulation((el_side,el_side),bc_dict,cuda=True)
+    
+    simresults = sim.simulation_run_for(0.01,100)
+    
+    # access the nonzero elements of k_mattrix als further input
+    k_mat = sim.k_mat.get()
+    mat_indcs = np.array(np.where(k_mat > 0.)).T
+    # trying to copy everywhere to get rid of the references
+    nonzero = k_mat[mat_indcs[:,0],mat_indcs[:,1]].copy()
+    
+    return simresults, nonzero
+    
+if __name__ == '__main__':
+    data_dict = {}
+    data_dict['features'] = []
+    data_dict['targets'] = []
+    
+    for i in range(4000):
+        
+        print(f'starting simulation {i}...')
+        res, nonzero_k_mat = data_gen()
+        feature = res['temperature'][0].copy()
+        target = res['temperature'][-1].copy()
+        data_dict['features'].append((feature,nonzero_k_mat))
+        data_dict['targets'].append(target)
+        print(f'finished simulation {i}')
+    
+        
+    # write all the data
+    with open('training_data_big.sml','wb') as pd:
+        pickle.dump(data_dict,pd)
